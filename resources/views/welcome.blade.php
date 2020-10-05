4d7e3a613c0616c036b9a2853114a60f82c65188
JairoPadillaN <al221910005@gmail.com>
2020-10-05 09:35:23 -0500

    proyecto personal

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+@section('content')
+<div class="container">
+    <div class="row">
+        <h1>Bienvenido a MiLaravel</h1>
+    </div>
+</div>
+@endsection
